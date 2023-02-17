b2b90b36f089835aed7b4b092bf817c6a6a5afa2
zyb <207239391@qq.com>
2023-02-17 15:37:42 +0800

    master commit2

--- src/main/java/com/zyb/admin/Boot07WebApplication.java
+++ src/main/java/com/zyb/admin/Boot07WebApplication.java
@@ -15,6 +15,7 @@ public class Boot07WebApplication {
         System.out.println("1");
         System.out.println("2");
         System.out.println("4");
+        System.out.println("master test");
     }
 
 }
